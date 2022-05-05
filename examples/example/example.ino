ce2c79f64436787b20202dc7356076aa33255a58
PB2 <gitstuff2@gmail.com>
2022-05-04 23:13:48 -0400

    Add new eval() function to example

--- examples/example/example.ino
+++ examples/example/example.ino
@@ -8,26 +8,35 @@ void setup()
   Serial.begin(115200);
   while(!Serial);
 
-  Vector2f mean;
+  Vector2d mean;
   mean << 0,
           0;
           
-  Matrix2f covar;
+  Matrix2d covar;
   covar << 10, 0,
             0, 1;
   
-  EigenMultivariateNormal<float> solver(mean, covar);
+  EigenMultivariateNormal<double> multVNorm(mean, covar);
 
   while(true)
   {
-    auto samples = solver.samples(5).transpose();
+    Serial.println("--------------------------------------------------");
+    
+    Matrix<double, 5, 2> samples = multVNorm.samples(5).transpose();
+    Serial.println("5 random samples drawn from the distribution:");
     Serial.print(samples(0, 0)); Serial.print(", "); Serial.println(samples(0, 1));
     Serial.print(samples(1, 0)); Serial.print(", "); Serial.println(samples(1, 1));
     Serial.print(samples(2, 0)); Serial.print(", "); Serial.println(samples(2, 1));
     Serial.print(samples(3, 0)); Serial.print(", "); Serial.println(samples(3, 1));
     Serial.print(samples(4, 0)); Serial.print(", "); Serial.println(samples(4, 1));
     Serial.println();
-    delay(100);
+
+    double evalMean = multVNorm.eval(mean);
+    Serial.println("Distribution evaluated at the mean location:");
+    Serial.println(evalMean);
+    Serial.println();
+    
+    delay(1000);
   }
 }
 
